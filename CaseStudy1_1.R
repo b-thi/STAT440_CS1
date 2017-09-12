57d25915d8e3a92dc38a81ca14e0ab775717c38b
Barinder Thind <b.thindd@gmail.com>
2017-09-12 15:33:19 -0700

    First script

---
+++ CaseStudy1_1.R
@@ -0,0 +1,9 @@
+# STAT 440 - Case Study 1 - Fall 2017#
+# Matthew Reyers, Brad Smallwood, Barinder Thind #
+# Code: September 12th, 2017
+
+# Loading relevant packages
+library(rJava)
+library(xlsx)
+library(tidyverse)
+
